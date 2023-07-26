1432c2875f6aed2aca5d9911242996cb9f1ed656
lutz <lutzfinke2010@gmail.com>
2023-07-26 13:29:58 +0200

    Initial commit

---
+++ src/main/java/de/fantasticcoders/copilotdojo/CopilotdojoApplication.java
@@ -0,0 +1,13 @@
+package de.fantasticcoders.copilotdojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopilotdojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CopilotdojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/fantasticcoders/copilotdojo/CopilotdojoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fantasticcoders.copilotdojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CopilotdojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
